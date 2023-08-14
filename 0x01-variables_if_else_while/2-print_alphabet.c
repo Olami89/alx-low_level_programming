e88cb5dd5ce18f6fb295473430bf816c7e388e12
olami89 <ibuoyeolamide8@gmail.com>
2023-08-14 13:19:29 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - alphabets
+ *
+ * Return: Always 0 (Successful)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+
+				{
+
+							putchar(ch);
+
+								}
+
+			putchar('\n');
+
+				return (0);
+}
